6a4d025db026917a2115c8c53fba033e2f97d557
JohannFonseca <arielfonseca049@gmail.com>
2024-11-08 20:38:50 -0600

    Login y main

---
+++ Login.java
@@ -0,0 +1,44 @@
+// aca es la ventana de Login
+
+//Librerias
+
+import javax.swing.*; 
+import java.awt.*;    
+public class Login extends JFrame {
+
+    // Constructor de la ventana
+    public Login() {
+
+        // Título de la ventana
+        setTitle("Login");
+
+        // Tamaño de la ventana
+        setSize(500, 500);
+
+        // Configuración para cerrar la ventana al presionar la 'X'
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLayout(null);
+
+        //agregar imagen
+        JLabel fondo = new JLabel(new ImageIcon("imagen.jpg")); 
+
+        // Ajusta el tamaño y posición para cubrir toda la ventana
+        fondo.setBounds(0, 0, 500, 500); 
+
+        // Agrega la imagen a la ventana
+        add(fondo); 
+
+        // Crear un botón
+        JButton botonLogin = new JButton("Iniciar Sesion");
+
+        //posicion y tamaño del boton
+        botonLogin.setBounds(200, 400, 100, 40);
+
+        // Agrega el botón encima del fondo de imagen
+        fondo.add(botonLogin); 
+
+        // Muestra la ventana centrada en la pantalla
+        setLocationRelativeTo(null);
+    }
+}
---
+++ Main.java
@@ -0,0 +1,13 @@
+
+// La main, simplente llama a la clase Inicio
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // Crear una instancia de la ventana Login
+        Login ventana = new Login();
+
+        // Hacer la ventana visible
+        ventana.setVisible(true);
+    }
+}
